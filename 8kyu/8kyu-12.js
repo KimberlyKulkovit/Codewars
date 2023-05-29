1f47c5993be6b56e65d5bad879f055d396629b92
KimberlyKulkovit <126214692+KimberlyKulkovit@users.noreply.github.com>
2023-05-29 11:28:41 -0700

    Create 8kyu-12.js
    Function 2 - Squaring an argument

---
+++ 8kyu/8kyu-12.js
@@ -0,0 +1,7 @@
+/*Function 2 - Squaring an argument 
+Now you have to write a function that takes an argument and returns the square of it.*/ 
+// Write the "square"-function here
+function square(num){ 
+  let numSquared = Math.pow(num, 2);
+  return numSquared;
+}
